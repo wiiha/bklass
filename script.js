e7ba0e55c26b5ba9321c0607aefc261dd3359883
Gustav Fridell <gustavfridell@me.com>
2016-11-09 22:30:23 +0100

    add different messages depending on number of seats available

--- script.js
+++ script.js
@@ -20,13 +20,25 @@ window.onload = function() {
             data: serializedData
         })
 
-        request.done(function (response, textStatus, jqXHR) {
+        request.done(function (response) {
+            var message = '';
             var mail = $form[0].elements.Epost.value;
-            $('#form-success #mail').text(mail);
+
+            if (response.result === 'success') {
+                if (response.allSeatsTaken) {
+                    message = 'Tyvärr är alla platser redan tagna. Du har fått en plats i reservlistan, så om en plats blir ledig hör vi av oss.';
+                } else {
+                    message = 'Snyggt! Ett mail har skickats till ' + mail + ' där det står hur du ska betala.';
+                }
+            } else {
+                mesasge = 'Woops, något gick fel!'
+            }
+
+            $('#form-success').text(message);
             $('#form-success').show();
         })
 
-        request.fail(function (jqXHR, textStatus, errorThrown) {
+        request.fail(function () {
             $('#form-failure').show();
         })
 

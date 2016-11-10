1ff46b927b56334301ae30e12f31b6992eeda7a7
Gustav Fridell <gustavfridell@me.com>
2016-11-10 20:41:46 +0100

    WIP

--- script.js
+++ script.js
@@ -20,30 +20,23 @@ window.onload = function() {
             data: serializedData
         })
 
-        request.done(function (response) {
-            var message = '';
-            var mail = $form[0].elements.Epost.value;
+        request.always(function (response) {
+            $form.hide();
 
             if (response.result === 'success') {
+                var message = '';
+                var mail = $form[0].elements.Epost.value;
                 if (response.allSeatsTaken) {
                     message = 'Tyvärr är alla platser redan tagna. Du har fått en plats i reservlistan, så om en plats blir ledig hör vi av oss.';
                 } else {
                     message = 'Snyggt! Ett mail har skickats till ' + mail + ' där det står hur du ska betala.';
                 }
+
+                $('#form-success').text(message);
+                $('#form-success').show();
             } else {
-                mesasge = 'Woops, något gick fel!'
+                $('#form-failure').show();
             }
-
-            $('#form-success').text(message);
-            $('#form-success').show();
-        })
-
-        request.fail(function () {
-            $('#form-failure').show();
-        })
-
-        request.always(function () {
-            $form.hide();
         })
     })
 }

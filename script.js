d02941f302f844a9deba34d5045997027fe910cb
Gustav Fridell <gustavfridell@me.com>
2016-11-10 20:58:06 +0100

    try and fix safari

--- script.js
+++ script.js
@@ -21,22 +21,22 @@ window.onload = function() {
         })
 
         request.always(function (response) {
+            var message = '';
+            var mail = $form[0].elements.Epost.value;
             $form.hide();
 
             if (response.result === 'success') {
-                var message = '';
-                var mail = $form[0].elements.Epost.value;
                 if (response.allSeatsTaken) {
                     message = 'Tyvärr är alla platser redan tagna. Du har fått en plats i reservlistan, så om en plats blir ledig hör vi av oss.';
                 } else {
                     message = 'Snyggt! Ett mail har skickats till ' + mail + ' där det står hur du ska betala.';
                 }
-
-                $('#form-success').text(message);
-                $('#form-success').show();
             } else {
-                $('#form-failure').show();
+                message = 'Snyggt! Om det finns platser kvar bör ett mail skickats till ' + mail + ' där det står hur du ska betala.';
             }
+
+            $('#form-success').text(message);
+            $('#form-success').show();
         })
     })
 }
